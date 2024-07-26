f67656f1a0ea1d590962de077dd9994ee51d76d1
Kyoduke <caioduque.dev@gmail.com>
2024-07-26 12:05:24 -0300

    changes settings

--- djangoapp/core/settings.py
+++ djangoapp/core/settings.py
@@ -190,5 +190,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'shacayou@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'zvmd edbz yals kcwr')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'vwqm bryu icay dqbv')
 EMAIL_USE_TLS = True 
