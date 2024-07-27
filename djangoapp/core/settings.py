c78b4f64304d7eb7b8fa0b57d80f405a0eb1ef0b
Kyoduke <caioduque.dev@gmail.com>
2024-07-27 15:10:18 -0300

    fix static and media constants

--- djangoapp/core/settings.py
+++ djangoapp/core/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [
 # STATIC_DIR = BASE_DIR / 'static'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
