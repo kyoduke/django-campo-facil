ae574d0378a2f2c00b940ce965acec6ae306b112
Kyoduke <caioduque.dev@gmail.com>
2024-07-30 08:22:57 -0300

    removes app_password from email configuration

--- djangoapp/core/settings.py
+++ djangoapp/core/settings.py
@@ -193,5 +193,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'shacayou@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'vwqm bryu icay dqbv')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True 
