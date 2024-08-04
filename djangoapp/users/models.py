56d7c25a7e8ed8fbdb1f47c0c4374a13535df996
Kyoduke <caioduque.dev@gmail.com>
2024-08-04 12:18:10 -0300

    added type definition to objects

--- djangoapp/users/models.py
+++ djangoapp/users/models.py
@@ -38,7 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now)
     last_login = models.DateTimeField(blank=True, null=True)
 
-    objects = CustomUserManager()
+    objects:CustomUserManager = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
