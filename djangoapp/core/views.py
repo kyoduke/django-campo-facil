6a60d7cea8c2e812f66b8077a15a2d6a108cf300
Kyoduke <caioduque.dev@gmail.com>
2024-08-05 22:55:04 -0300

    added required login to root url

--- djangoapp/core/views.py
+++ djangoapp/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(redirect_field_name='account_login')
 def home_page(request):
     return render(request, 'core/home.html')
