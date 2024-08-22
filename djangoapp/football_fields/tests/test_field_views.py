ec6c80613815c6c09a2ea04fa2806d4f287eb765
Kyoduke <caioduque.dev@gmail.com>
2024-08-22 12:14:46 -0300

    added tests for views

--- djangoapp/football_fields/tests/test_field_views.py
+++ djangoapp/football_fields/tests/test_field_views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import Client
 from django.core.files.uploadedfile import SimpleUploadedFile
-from football_fields.models import FootballField
+from football_fields.models import FootballField, Address
+from django.urls import reverse
+from decimal import Decimal
 import pytest
 import os
 
@@ -34,6 +36,15 @@ class TestFootballFieldViews():
         yield data
 
     @pytest.fixture
+    def football_field_fixture(self):
+        yield FootballField.objects.create(name='campinhola', hour_price=240, has_changing_room=True, has_field_lighting=True)
+
+    @pytest.fixture()
+    def address_fixture(self, football_field_fixture):
+        yield Address.objects.create(football_field=football_field_fixture, address_one='R. Dois', state='RJ', city='Maricá', district='Itaipuaçu', cep_code='24999-392', latitude=Decimal('-12.42'), longitude=Decimal('-12.42'))
+
+
+    @pytest.fixture
     def user_fixture(self):
         yield User.objects.create_user(email='test_views@views.com', password='abc123')
 
@@ -53,6 +64,20 @@ class TestFootballFieldViews():
 
         assert logged is True
         assert response.status_code == 200
+
+    def test_if_context_sends_lat_long(self, client: Client, logged, address_fixture):
+        """
+        tests if view is sending latitude and longitude in the context
+        """
+        response = client.get('/fields/')
+        context = response.context[0]
+        data = []
+        for i in context:
+            if 'addresses' in i:
+                data = i['addresses']
+        assert str(address_fixture.latitude) in data
+        assert str(address_fixture.longitude) in data
+
 
     def test_post_invalid_data(self, client: Client, logged):
         response = client.post('/fields/new')
@@ -79,6 +104,20 @@ class TestFootballFieldViews():
     def test_is_form_rendered(self, client: Client, logged):
         response = client.get('/fields/new')
         content = response.content.decode('utf-8')
-        print(content)
+
         assert response.status_code == 200
         assert '<input type="text"' in content
+    
+    def test_detail_if_field_exists(self, client: Client, logged, football_field_fixture):
+        url = reverse('football_field_detail', args=[football_field_fixture.pk])
+        response = client.get(url)
+        content = response.content.decode('utf-8')
+        
+        assert football_field_fixture.name in content
+
+    def test_detail_if_field_does_not_exists(self, client:Client, logged):
+        url = reverse('football_field_detail', args=[999])
+        response = client.get(url)
+
+        assert response.status_code == 302
+        assert response.url == '/fields/'
