3831b892ab9e3c6f82b72555e1a72377ea751131
Kyoduke <caioduque.dev@gmail.com>
2024-08-27 09:26:43 -0300

    removed bootstrap5 module and changed styling on navbar

--- djangoapp/users/forms.py
+++ djangoapp/users/forms.py
@@ -7,11 +7,10 @@ class UpdateProfileForm(forms.ModelForm):
     profile_image = forms.ImageField(required=False, label='Foto de perfil')
     first_name = forms.CharField(max_length=100, required=False, label='Nome')
     last_name = forms.CharField(max_length=100, required=False, label='Sobrenome')
-    email = forms.EmailField(required=True)
     phone_number = PhoneNumberField(region='BR', required=False, label='Celular')
 
 
     class Meta:
         model = get_user_model()
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'profile_image']
+        fields = ['first_name', 'last_name','phone_number', 'profile_image']
         
