3dcf44ae9cca23db64fe1c290921519a7e1a28c8
Kyoduke <caioduque.dev@gmail.com>
2024-09-10 10:02:03 -0300

    added all states to model choice field

--- djangoapp/football_fields/models.py
+++ djangoapp/football_fields/models.py
@@ -53,9 +53,33 @@ class FootballField(models.Model):
 class Address(models.Model):
 
     STATE_CHOICES = [
-        ("RJ", "Rio de Janeiro"),
-        ("SP", "São Paulo"),
+        ("AC", "Acre"),
+        ("AL", "Alagoas"),
+        ("AP", "Amapá"),
+        ("AM", "Amazonas"),
+        ("BA", "Bahia"),
+        ("CE", "Ceará"),
+        ("DF", "Distrito Federal"),
+        ("ES", "Espírito Santo"),
+        ("GO", "Goiás"),
+        ("MA", "Maranhão"),
+        ("MT", "Mato Grosso"),
+        ("MS", "Mato Grosso do Sul"),
         ("MG", "Minas Gerais"),
+        ("PA", "Pará"),
+        ("PB", "Paraíba"),
+        ("PR", "Paraná"),
+        ("PE", "Pernambuco"),
+        ("PI", "Piauí"),
+        ("RJ", "Rio de Janeiro"),
+        ("RN", "Rio Grande do Norte"),
+        ("RS", "Rio Grande do Sul"),
+        ("RO", "Rondônia"),
+        ("RR", "Roraima"),
+        ("SC", "Santa Catarina"),
+        ("SP", "São Paulo"),
+        ("SE", "Sergipe"),
+        ("TO", "Tocantins"),
     ]
 
     football_field = models.OneToOneField(
